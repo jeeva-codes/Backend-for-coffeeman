0cee9c1a04a0f00070a403fe7b23e53d79716a19
jeeva-codes <jeevacode2@gmail.com>
2025-09-02 09:14:20 -0800

    test

--- api/index.js
+++ api/index.js
@@ -12,9 +12,13 @@ app.use(cors({
   credentials: false
 }))
 
-mongoose.connect(process.env.MONGO_URI)
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+})
 .then(() => console.log("MongoDB connected"))
 .catch(err => console.error("MongoDB error:", err))
+
 const UserSchema = new mongoose.Schema({
   name: String,
   email: String,
